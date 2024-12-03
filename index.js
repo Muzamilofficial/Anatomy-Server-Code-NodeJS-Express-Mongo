f81f82c54f2f7a3dd42700ecb1289d251c53d3d8
Muzamil Khan <113015136+Muzamilofficial@users.noreply.github.com>
2024-12-03 09:47:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ app.get("/login-success", (req, res) => {
           <p>An email has been sent to your Google account <strong>${email}</strong> with your login credentials.</p>
           <p>Please check your inbox to continue. If you haven't received the email, kindly check your spam folder or try again later.</p>
           
-          <a href="/" class="btn">Continue</a> <!-- Continue button that redirects to another page -->
+          <a href="/new" class="btn">Continue</a> <!-- Continue button that redirects to another page -->
 
           <div class="footer">&copy; 2024 Anatomy. All rights reserved.</div>
         </div>
@@ -306,7 +306,9 @@ app.get("/login-success", (req, res) => {
 });
 
 
-
+app.post("/new", async (req, res) => {
+console.log('lol');
+});
 
 
 
