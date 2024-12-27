55e2271496b0cff2c8875e5b5dcd49180da7bd0c
Muzamil Khan <113015136+Muzamilofficial@users.noreply.github.com>
2024-12-27 05:46:59 -0800

    Update index.js
    Update index.js

--- index.js
+++ index.js
@@ -1308,6 +1308,18 @@ app.post('/update-password', async (req, res) => {
 });
 
 
+app.get("/user/:email", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.params.email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user", error });
+  }
+});
+
 
 // Start the server
 app.listen(PORT, () => {
